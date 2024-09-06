6d779d00eed1b4af597f49d2c92a85018888f119
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-09-06 14:14:34 -0300

    Adicionado a classe Conta

--- obj/Debug/netcoreapp3.1/ContaBancaria.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/ContaBancaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b63e18ae49d2acc14f123f08821f18c87a42ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d520ccb35d5c4aefc1db18f998ee8912097ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
